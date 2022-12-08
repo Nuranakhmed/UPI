1a9e7b7542c04b2d2734ad77d8e97418db862a4c
abdulayef1 <7ka1cyr>
2022-12-08 12:43:49 +0400

    n

---
+++ ConsoleApp4/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }    
+        public string Zipcode { get; set; }
+        public Geo Geo { get; set; }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using ConsoleApp4;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+
+List<UserInfo>  response = await GetAsync("https://jsonplaceholder.typicode.com/users");
+foreach (var item in response)
+{
+    Console.WriteLine(item.Id);
+}
+
+
+static async Task<List<UserInfo>> GetAsync(string path)
+{
+    List<UserInfo> post = default;
+    using(HttpClient client = new HttpClient())
+    {
+        var responseMessage= await client.GetAsync(path);
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var responseString=await responseMessage.Content.ReadAsStringAsync();
+            post = JsonConvert.DeserializeObject<List<UserInfo>>(responseString);
+
+        }
+        else
+        {
+            throw new Exception();
+        }
+    }
+    return post;    
+}
+
+
+
+
